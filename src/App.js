ed6e5cee11b9e64511135492afc0994c3882e268
Oswaldo Ravelo <raveloswaldo@gmail.com>
2022-10-29 13:41:57 -0300

    understanding jsx

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const name ='Matheus';
+
+  const newName = name.toUpperCase()
+
+  function soma(a,b){
+    return a + b;
+  }
+
+  const url='https://s1.static.brasilescola.uol.com.br/be/conteudo/images/imagem-em-lente-convexa.jpg'
   return (
     <div className="App">
-      <h2>Hello, React</h2>
+      <h1>Hello, {name}</h1>
+      <h2>Hello, {newName}</h2>
+      <p>Soma: {2+2}</p>
+      <p>Soma: {soma(1,2)}</p>
+      <img src={url} alt='Minha imagem' />
+
     </div>
   );
 }
