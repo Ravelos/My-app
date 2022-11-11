e762f8a7ad2de1bf708a2db13a6c8b93358d9bcd
Oswaldo Ravelo <raveloswaldo@gmail.com>
2022-11-11 19:38:59 -0300

    adding a new component with fragments

---
+++ src/components/List.js
@@ -0,0 +1,16 @@
+function List(){
+    return(
+        <>
+            <h1>My List </h1>
+                <ul>
+                    <li>
+                        Item 1
+                    </li>
+                    <li>
+                        item 2
+                    </li>
+                </ul>            
+        </>
+    )
+}
+export default List
