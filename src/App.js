85fb4036e36185d4d6c2228c8ee7f64476b6f5fa
Oswaldo Ravelo <raveloswaldo@gmail.com>
2022-11-11 19:40:11 -0300

    using the list component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import SayMyName from './components/SayMyName';
 import Person from './components/Person';
 import Phrase from './components/Phrase';
+import List from './components/List';
 
 function App() {
 
@@ -20,6 +21,7 @@ function App() {
       profession = "Analist"
       picture = "https://s2.glbimg.com/3fuYf90x9fdl1KC1J_vcdv3y4D8=/600x400/smart/e.glbimg.com/og/ed/f/original/2022/08/02/gettyimages-1412406808.jpg"
       />
+      <List/>
     </div>
   );
 }
