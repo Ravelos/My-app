8a2f4e908f2e7ba0c1ad84c59fe641e98fb4079f
Oswaldo Ravelo <raveloswaldo@gmail.com>
2022-11-11 21:25:25 -0300

    using props in a different way

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,8 +1,22 @@
-function Item(props){
+import PropTypes from 'prop-types'
+
+function Item({brand,release_year}){
     return(
         <>
-        <li>{props.brand}</li>
+            <li>
+                {brand} - {release_year}
+            </li>
         </>
     )
+}
+
+Item.propType={
+    brand: PropTypes.string.isRequired,
+    release_year: PropTypes.number,
+}
+
+Item.defaultProps={
+    brand: 'The brand was not specified',
+    release_year: 0,
 }
 export default Item
