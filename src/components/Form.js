39c9946953bc1a7132c28f0d057cf0a618d51c44
Oswaldo Ravelo <raveloswaldo@gmail.com>
2022-11-12 12:06:24 -0300

    working useState in form component

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,42 @@
+import {useState} from 'react'
+
 function Form(){
     function registerUser(e){
         e.preventDefault()
-        console.log('The user was registered')
+        // console.log(name)
+        // console.log(password)
+        // console.log('The user was registered')
+        console.log(`user ${name} was register with this ${password}`)
     }
+
+    const [name, setName] = useState()
+    const [password, setPassword] = useState()
+
+
     return(
         <div>
             <h1>Register</h1>
             <form onSubmit={registerUser}>
                 <div>
-                    <input type="text" placeholder="Type your name"/>
+                    <label htmlFor="name">Name:</label>
+                    <input
+                      type="text" 
+                      id="name" 
+                      name="name" 
+                      placeholder="Type your name"
+                      //value={name}
+                      onChange={(e) => setName(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password">Password:</label>
+                    <input 
+                     type="password" 
+                     id="password" 
+                     name="password" 
+                     placeholder="Enter your password"
+                     onChange={(e)=>setPassword(e.target.value)}
+                    />
                 </div>
                 <div>
                 <input type="submit" value="register"/>
