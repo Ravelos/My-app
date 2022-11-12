b80e257d848f607d4188e89e907368ced4d78902
Oswaldo Ravelo <raveloswaldo@gmail.com>
2022-11-11 21:26:11 -0300

    testing the changes into list component

--- src/components/List.js
+++ src/components/List.js
@@ -5,9 +5,10 @@ function List(){
         <>
             <h1>My List </h1>
                 <ul>
-                    <Item brand='Ferrari'/>
-                    <Item brand='Fiat'/>
-                    <Item brand='Renault'/>
+                    <Item brand='Ferrari' release_year={1985}/>
+                    <Item brand='Fiat' release_year={2000}/>
+                    <Item brand='Renault' release_year={2018}/>
+                    <Item/>
                 </ul>            
         </>
     )
