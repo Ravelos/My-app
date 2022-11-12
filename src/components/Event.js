2f14d2df66635e086dfff92e37a527cdc5c1f39e
Oswaldo Ravelo <raveloswaldo@gmail.com>
2022-11-12 12:34:03 -0300

    creating event componet based on props

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,13 +1,20 @@
+import Button from "./event/Button"
+
 function Event({number}){
 
     function myEvent(){
-        console.log(`I was activated ${number}`)
+        console.log('activating first event')
+    }
+
+    function secondEvent(){
+        console.log('activating second event')
     }
 
     return(
         <div>
             <p>Click to trigger an event</p>
-            <button onClick={myEvent}>Activate</button>
+            <Button event={myEvent} text="First event"/>
+            <Button event={secondEvent} text="Second event"/>
         </div>
 
     )
