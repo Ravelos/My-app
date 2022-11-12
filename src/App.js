809c196ed190cbaff61cff2a607c8607a05f2e4d
Oswaldo Ravelo <raveloswaldo@gmail.com>
2022-11-12 11:24:16 -0300

    adding the form and event component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
 import './App.css';
-import SayMyName from './components/SayMyName';
-import Person from './components/Person';
-import Phrase from './components/Phrase';
-import List from './components/List';
+import Event from './components/Event';
+import Form from './components/Form';
 
 function App() {
 
-  const name1 = 'Jesus';
 
   return (
     <div className="App">
-      <Phrase/>
-      <Phrase/>
-      <SayMyName name="Matheus" />
-      <SayMyName name="Robert" />
-      <SayMyName name={name1} />
-      <Person 
-      name = "Julio"
-      age = "45"
-      profession = "Analist"
-      picture = "https://s2.glbimg.com/3fuYf90x9fdl1KC1J_vcdv3y4D8=/600x400/smart/e.glbimg.com/og/ed/f/original/2022/08/02/gettyimages-1412406808.jpg"
-      />
-      <List/>
+      <h1>Testing events</h1>
+      <Event number="1"/>
+      <Event number="2"/>
+      <Form/>  
     </div>
   );
 }
