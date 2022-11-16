9cbdfd0d3b0ea5b928911383b262261ad3560859
Oswaldo Ravelo <raveloswaldo@gmail.com>
2022-11-15 21:47:58 -0300

    using the routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import './App.css';
-import {useState} from 'react'
-import YourName from './components/YourName';
-import Greetings from './components/Greetings';
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Company from './pages/Company'
+import ContactUs from './pages/ContactUs'
+import NavBar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
 
 function App() {
 
-  const [name, setName]=useState()
-
   return (
-    <div className="App">
-      <h1>State Lift</h1>
-       <YourName setName={setName} />
-       <Greetings name={name}/>
-    </div>
-  );
+    <Router>
+      <NavBar/>
+      <Routes>
+      <Route path="/" element={<Home />} > </Route>
+      <Route path="/company" element={<Company />} > </Route>
+      <Route path="/contact" element={<ContactUs />} > </Route>
+      </Routes>
+      <Footer/>
+    </Router>
+    
+  )
 }
 
 export default App;
