229328950971ae3d96fd98a7a6965a150fac8d66
Oswaldo Ravelo <raveloswaldo@gmail.com>
2022-12-08 00:35:50 -0300

    includes the new components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
-import Home from './pages/Home'
-import Company from './pages/Company'
-import ContactUs from './pages/ContactUs'
+import Home from './components/pages/Home'
+import Company from './components/pages/Company'
+import ContactUs from './components/pages/ContactUs'
 import NavBar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
 
