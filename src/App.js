202bd8f34cddc3f3c14657601cdee2483c6a86a7
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-02-08 17:19:43 -0300

    refactors the code

--- src/App.js
+++ src/App.js
@@ -1,27 +1,7 @@
-import './App.css';
-import SayMyName from './components/SayMyName';
-import Person from './components/Person';
-import Phrase from './components/Phrase';
-import List from './components/List';
-
 function App() {
-
-  const name1 = 'Jesus';
-
   return (
     <div className="App">
-      <Phrase/>
-      <Phrase/>
-      <SayMyName name="Matheus" />
-      <SayMyName name="Robert" />
-      <SayMyName name={name1} />
-      <Person 
-      name = "Julio"
-      age = "45"
-      profession = "Analist"
-      picture = "https://s2.glbimg.com/3fuYf90x9fdl1KC1J_vcdv3y4D8=/600x400/smart/e.glbimg.com/og/ed/f/original/2022/08/02/gettyimages-1412406808.jpg"
-      />
-      <List/>
+      <p>Costs</p>
     </div>
   );
 }
