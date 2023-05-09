260108bd0b55115bfb49842051cbf6f9fbfe7bbe
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-05-08 22:01:25 -0300

    Sets up the home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,9 +1,16 @@
+import styles from './Home.module.css'
+import savings from '../../img/savings.svg'
+import LinkButton from '../layout/LinkButton'
 function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  );
+    <section className={styles.home_container}>
+      <h1> Welcome to <span>Costs</span></h1>
+      <p>Start to manage your projects right away!</p>
+      <LinkButton to="/newproject" text="Create a project" />
+      <img src={savings} alt='Costs'/>
+    </section>
+ 
+  )
 }
 
 export default Home;
