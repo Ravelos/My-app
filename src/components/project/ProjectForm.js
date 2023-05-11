d77be933f83b116f40f2b397ee4aba1863a188b3
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-05-10 23:09:48 -0300

    Create the template for the component

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,22 +1,27 @@
-function ProjectForm(){
-    return (
-        <form>
-            <div>
-                <input type="text" placeholder="Type the project's name"/>
-            </div>
-            <div>
-                <input type="text" placeholder="Set a budget"/>
-            </div>
-            <select>
-                <option disabled>Select category</option>    
-            </select>
-            <div>
-                <input type="submit" value="Create project"/>
-            </div>         
-            
-            
-        </form>
-    )
+import styles from "./ProjectForm.module.css";
+import Input from "../form/Input";
+import Select from "../form/Select";
+import SubmitButton from "../form/SubmitButton";
+
+function ProjectForm({ btnText }) {
+  return (
+    <form className={styles.form}>
+      <Input
+        type="text"
+        text="Project's name"
+        name="name"
+        placeholder="Type project's name"
+      />
+      <Input
+        type="number"
+        text="Project's estimated budget"
+        name="budget"
+        placeholder="Type project's total budget"
+      />
+      <Select name="category id" text="Select a category" />
+      <SubmitButton text={btnText} />
+    </form>
+  );
 }
 
-export default ProjectForm
+export default ProjectForm;
