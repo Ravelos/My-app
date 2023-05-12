bc68813d350ac0425c13f607b739aef46b6f0f14
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-05-12 15:16:08 -0300

    Creates submit method

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -4,8 +4,9 @@ import Input from "../form/Input";
 import Select from "../form/Select";
 import SubmitButton from "../form/SubmitButton";
 
-function ProjectForm({ btnText }) {
+function ProjectForm({ handleSubmit, btnText, projectData }) {
   const [categories, setCategories] = useState([]);
+  const [project, setProject] = useState(projectData || {});
 
   useEffect(() => {
     fetch("http://localhost:5000/categories", {
@@ -21,24 +22,49 @@ function ProjectForm({ btnText }) {
       .catch((err) => console.log(err));
   }, []);
 
+  const submit = (e) => {
+    e.preventDefault();
+    handleSubmit(project);
+  };
+
+  function handleChange(e) {
+    setProject({ ...project, [e.target]: e.target.value });
+  }
+
+  function handleCategory(e) {
+    setProject({
+      ...project,
+      category: {
+        id: e.target.value,
+        name: e.target.options[e.target.selectedIndex].text,
+      },
+    });
+  }
+
   return (
-    <form className={styles.form}>
+    <form onSubmit={submit} className={styles.form}>
       <Input
         type="text"
         text="Project's name"
         name="name"
         placeholder="Type project's name"
+        handleOnChange={handleChange}
+        value= {project.name ? project.name: ''}
       />
       <Input
         type="number"
         text="Project's estimated budget"
         name="budget"
         placeholder="Type project's total budget"
+        handleOnChange={handleChange}
+        value={project.budget ? project.budget:''}
       />
       <Select
         name="category id"
         text="Select a category"
         options={categories}
+        handleOnChange={handleCategory}
+        value={project.category ? project.category.id : ""}
       />
       <SubmitButton text={btnText} />
     </form>
