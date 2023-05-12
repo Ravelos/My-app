2e3005900b210279a822e187230f3604a11d17a4
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-05-12 11:14:50 -0300

    Implements the hook useEffect for handling the request to the database

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,9 +1,26 @@
+import { useEffect, useState } from "react";
 import styles from "./ProjectForm.module.css";
 import Input from "../form/Input";
 import Select from "../form/Select";
 import SubmitButton from "../form/SubmitButton";
 
 function ProjectForm({ btnText }) {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/categories", {
+      method: "GET",
+      headers: {
+        "Content-Type": "aplication/json",
+      },
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setCategories(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <form className={styles.form}>
       <Input
@@ -18,7 +35,11 @@ function ProjectForm({ btnText }) {
         name="budget"
         placeholder="Type project's total budget"
       />
-      <Select name="category id" text="Select a category" />
+      <Select
+        name="category id"
+        text="Select a category"
+        options={categories}
+      />
       <SubmitButton text={btnText} />
     </form>
   );
