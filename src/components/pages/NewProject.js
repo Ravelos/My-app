ce3146830de6ab88a844ee364d3ea480d52fc52e
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-05-12 15:15:35 -0300

    Changes history to navigate from react-router-dom

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,12 +1,35 @@
+import {useNavigate} from 'react-router-dom'
 import ProjectForm from '../project/ProjectForm';
 import styles from './NewProject.module.css'
 
 function NewProject() {
+  
+  const history = useNavigate()
+
+  function createPost(project){
+    //Initialize cost and services
+    project.cost = 0;
+    project.services = [];
+
+    fetch('htpp://localhost:5000/projects', {
+      method: 'POST',
+      header: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(project),
+    })
+    .then((resp) => resp.json())
+    .then((data) => {
+      console.log(data);
+      history('/projects', {message:'Project successfully sent'})
+    })
+    .catch((err) => console.log(err))
+  }
   return (
     <div className={styles.newproject_container}>
       <h1>Create your Project</h1>
       <p>Create a new project and then add the services</p>
-      <ProjectForm btnText="Create project"/>
+      <ProjectForm handleSubmit={createPost} btnText="Create project"/>
     </div>
   );
 }
