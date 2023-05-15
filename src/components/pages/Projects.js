dad0b632c9028dafe57f17fcb9871474abf1d6e9
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-05-14 22:28:32 -0300

    Adds projectcard component

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,33 +1,58 @@
-import {useLocation} from 'react-router-dom'
+import { useLocation } from "react-router-dom";
 
 import Message from "../layout/Message";
-import Container from '../layout/Container'
-import LinkButton from '../layout/LinkButton';
+import Container from "../layout/Container";
+import LinkButton from "../layout/LinkButton";
+import ProjectCard from "../project/ProjectCard";
 
-import styles from './Projects.module.css'
+import styles from "./Projects.module.css";
+import { useState, useEffect } from "react";
 
 function Projects() {
+  const [projects, setProject] = useState([]);
 
-  const location = useLocation()
-  let message = ''
+  const location = useLocation();
+  let message = "";
 
-  if(location.state){
-    message = location.state.message
+  if (location.state) {
+    message = location.state.message;
   }
 
+  useEffect(() => {
+    fetch("http://localhost:5000/projects", {
+      method: "GET",
+      headers: {
+        "Content-type": "application/json",
+      },
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setProject(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
-    return (
-      <div className={styles.project_container}>
-        <div className={styles.title_container}>
+  return (
+    <div className={styles.project_container}>
+      <div className={styles.title_container}>
         <h1>My projects</h1>
-      <LinkButton to="/newproject" text="Create a project" />
-
-        </div>
-        {message && <Message type="success" msg={message}/>}
-        <Container customClass="start">
-        </Container>
+        <LinkButton to="/newproject" text="Create a project" />
       </div>
-    );
-  }
-  
-  export default Projects;
+      {message && <Message type="success" msg={message} />}
+      <Container customClass="start">
+        {projects.length > 0 &&
+          projects.map((project) => (
+            <ProjectCard
+              id={project.id}
+              name={project.name}
+              budget={project.budget}
+              category={project.category.name}
+              key={project.id}
+            />
+          ))}
+      </Container>
+    </div>
+  );
+}
+
+export default Projects;
